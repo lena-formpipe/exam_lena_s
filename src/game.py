ff5ff75210511220cf579ce0b5a2b870e67e533d
David Andersson <lejonmanen@gmail.com>
2026-06-22 11:15:29 +0200

    Easier to start

--- src/game.py
+++ src/game.py
@@ -1,47 +1,59 @@
-from .grid import Grid
-from .player import Player
-from . import pickups
-
-
-
-player = Player(2, 1)
-score = 0
-inventory = []
-
-g = Grid()
-g.set_player(player)
-g.make_walls()
-pickups.randomize(g)
+from src.grid import Grid
+from src.player import Player
+from src import pickups
 
 
 # TODO: flytta denna till en annan fil
-def print_status(game_grid):
+class GameState:
+    """Samla spelets variabler i en klass."""
+    def __init__(self):
+        self.player = Player(2, 1)
+        self.score = 0
+        self.inventory = []
+
+        self.g = Grid()
+        self.g.set_player(self.player)
+        self.g.make_walls()
+        pickups.randomize(self.g)
+
+
+# TODO: flytta denna till en annan fil
+def print_status(game_grid, state):
     """Visa spelvärlden och antal poäng."""
     print("--------------------------------------")
-    print(f"You have {score} points.")
+    print(f"You have {state.score} points.")
     print(game_grid)
 
 
-command = "a"
-# Loopa tills användaren trycker Q eller X.
-while not command.casefold() in ["q", "x"]:
-    print_status(g)
+def start(state):
+    command = "a"
+    # Loopa tills användaren trycker Q eller X.
+    while not command.casefold() in ["q", "x"]:
+        print_status(state.g, state)
 
-    command = input("Use WASD to move, Q/X to quit. ")
-    command = command.casefold()[:1]
+        command = input("Use WASD to move, Q/X to quit. ")
+        command = command.casefold()[:1]
 
-    if command == "d" and player.can_move(1, 0, g):  # move right
-        # TODO: skapa funktioner, så vi inte behöver upprepa så mycket kod för riktningarna "W,A,S"
-        maybe_item = g.get(player.pos_x + 1, player.pos_y)
-        player.move(1, 0)
+        if command == "d" and state.player.can_move(1, 0, state.g):  # move right
+            # TODO: skapa funktioner, så vi inte behöver upprepa så mycket kod för riktningarna "W,A,S"
+            maybe_item = state.g.get(state.player.pos_x + 1, state.player.pos_y)
+            state.player.move(1, 0)
 
-        if isinstance(maybe_item, pickups.Item):
-            # we found something
-            score += maybe_item.value
-            print(f"You found a {maybe_item.name}, +{maybe_item.value} points.")
-            #g.set(player.pos_x, player.pos_y, g.empty)
-            g.clear(player.pos_x, player.pos_y)
+            if isinstance(maybe_item, pickups.Item):
+                # we found something
+                state.score += maybe_item.value
+                print(f"You found a {maybe_item.name}, +{maybe_item.value} points.")
+                #g.set(player.pos_x, player.pos_y, g.empty)
+                state.g.clear(state.player.pos_x, state.player.pos_y)
 
 
-# Hit kommer vi när while-loopen slutar
-print("Thank you for playing!")
+    # Hit kommer vi när while-loopen slutar
+    print("Thank you for playing!")
+
+
+# __name__ skapas av Python och sätts till "__main__" om man startar game.py
+# direkt. Detta är för att undvika att start-funktionen körs om man importerar
+# saker från game.py i en annan fil, till exempel vid testning.
+if __name__ == "__main__":
+    game_state = GameState()
+    start(game_state)
