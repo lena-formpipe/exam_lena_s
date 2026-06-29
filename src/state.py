b924b6a8c3c9bf3fba6edb29e544f5950f9d87ce
Lena S <lena.i.sandberg@gmail.com>
2026-06-29 19:11:51 +0200

    F.(refaktorerat lite) "i", skriver ut innehållet i spelarens inventory.

--- src/state.py
+++ src/state.py
@@ -1,8 +1,6 @@
+from src.player import *
 from src.grid import *
-from src.player import *  # Player
 from src.pickups import *
-
-
 
 # KLART: Flyttat denna klass till en annan fil "state.py"
 class GameState:
@@ -20,36 +18,37 @@ class GameState:
         self.g.make_walls()
         randomize(self.g) # pickups.randomize(self.g)
 
+
     # G. The floor is lava - för varje steg man går ska man tappa 1 poäng.
     def floor_lava(self):
         self.score -= 1
         print(f"You have {self.score} points.")
 
+
     # F. Nytt kommando: "i", skriver ut innehållet i spelarens inventory.
     def print_inventory(self):
-        print("Längden på listan är:")
-        print(len(self.inventory))
         if len(self.inventory) == 0:
             print("Inga föremål i listan.")
         elif len(self.inventory) > 0:
-            print("Det finns föremål i listan: ")
-            for item in self.inventory:
-                print(item)
+            print("Det finns följande föremål i listan: ")
+            print(", ".join(self.inventory))
+
+
     # parameter in i metoden är antal steg som ska flyttas i riktning x eller y
-    def find_item(self, x, y):
+    def move_player(self, x, y):
         # Kontrollera g (grid)
         maybe_item = self.g.get(self.player.pos_x+x, self.player.pos_y+y)
         self.player.move(x, y)
-        # G. The floor is lava - för varje steg man går ska man tappa 1 poäng.
+        # G. The floor is lava, för varje steg man går ska man tappa 1 poäng.
         self.floor_lava()
 
-        if isinstance(maybe_item, Item):  # isinstance(maybe_item, pickups.Item):
+        if isinstance(maybe_item, Item):
             # we found something, add the value to score
             self.score += maybe_item.value
-            print(f"You found a {maybe_item.name}, +{maybe_item.value} points.")
-            # LS E.	Inventory - alla saker som man plockar upp ska sparas i en lista.
-            self.inventory.append({maybe_item.name})
-            # g.set(player.pos_x, player.pos_y, g.empty) TODO vad var detta?
+            print(f"Du har hittat {maybe_item.name}, +{maybe_item.value} poäng.")
+            # E. Inventory - alla saker som man plockar upp ska sparas i en lista.
+            self.inventory.append(maybe_item.name)
+            """Ta bort item från position"""
             self.g.clear(self.player.pos_x, self.player.pos_y)
 
 
@@ -57,7 +56,7 @@ class GameState:
 def print_status(game_grid, state):
     """Visa spelvärlden och antal poäng."""
     print("--------------------------------------")
-    print(f"You have {state.score} points.")
+    print(f"Du har {state.score} poäng.")
     print(game_grid)
 
 
